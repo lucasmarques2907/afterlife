ce26177366c020dcb39b84e80adb614c55d8b340
lucasmarques2907 <lucasredra@gmail.com>
2024-06-25 21:39:38 -0300

    Adicionando Navbar das páginas adicionais

---
+++ frontend/src/components/gerais/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <v-app-bar>
+            <v-app-bar-title>
+                <a class="navbar-brand" href="#">
+                    <img src="../../assets/imgs/AfterlifeLogoCompleta.jpeg" class="pt-1" alt="Afterlife Logo">
+                </a>
+            </v-app-bar-title>
+            <v-menu>
+                <template v-slot:activator="{ props }">
+                    <v-btn icon="mdi-account" v-bind="props">
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item v-for="(item, index) in items" :key="index" @click="opcaoSelecionada(index)">
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            drawer: false,
+            items: [
+                {
+                    title: 'Configurar conta',
+                },
+                {
+                    title: 'Sair',
+                }
+            ]
+        };
+    },
+    computed: {
+        isMobile() {
+            return this.$vuetify.display.smAndDown;
+        },
+        drawerLocation() {
+            return this.isMobile ? 'top' : false;
+        },
+    },
+    methods: {
+        toggleDrawer() {
+            this.drawer = !this.drawer;
+        },
+        opcaoSelecionada(index){
+            let opcao = this.items[index].title;
+            switch (opcao){
+                case 'Configurar conta':
+                    console.log('Selecionou configurar conta')
+                    break
+                case 'Sair':
+                    console.log('Selecionou sair')
+                    break
+            }
+        }
+    },
+}
+</script>
+
+<style>
+.navbar-brand img {
+    max-width: 200px;
+}
+</style>
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
+        <Navbar />
         <Footer />
     </div>
 </template>
 
 <script>
+import Navbar from "../components/gerais/Navbar.vue";
 import Footer from "../components/gerais/Footer.vue";
 
 export default {
     name: 'viewAdmin',
-    components:{Footer}
+    components:{Navbar,Footer}
 }
 </script>
 
