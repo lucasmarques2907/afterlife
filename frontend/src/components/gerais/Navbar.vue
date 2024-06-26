f7a12ce859e9805569bead188ea128ae64eaa8a9
lucasmarques2907 <lucasredra@gmail.com>
2024-06-26 19:55:54 -0300

    Navbar e Footer das páginas 'gerais' finalizados

--- frontend/src/components/gerais/Navbar.vue
+++ frontend/src/components/gerais/Navbar.vue
@@ -13,8 +13,12 @@
                 </template>
 
                 <v-list>
-                    <v-list-item v-for="(item, index) in items" :key="index" @click="opcaoSelecionada(index)">
-                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    <v-list-item v-for="(item, index) in items" :key="index" value="item"
+                        @click="opcaoSelecionada(index)">
+                        <template v-slot:prepend>
+                            <v-icon :icon="item.icon" color="#4d6279"></v-icon>
+                        </template>
+                        <v-list-item-title> {{ item.title }}</v-list-item-title>
                     </v-list-item>
                 </v-list>
             </v-menu>
@@ -24,6 +28,9 @@
 
 <script>
 
+import router from '@/router';
+import Swal from 'sweetalert2';
+
 export default {
     name: 'Navbar',
     data() {
@@ -32,9 +39,13 @@ export default {
             items: [
                 {
                     title: 'Configurar conta',
+                    icon: 'mdi-cog',
+
                 },
                 {
                     title: 'Sair',
+                    icon: 'mdi-logout',
+
                 }
             ]
         };
@@ -51,14 +62,39 @@ export default {
         toggleDrawer() {
             this.drawer = !this.drawer;
         },
-        opcaoSelecionada(index){
+        opcaoSelecionada(index) {
             let opcao = this.items[index].title;
-            switch (opcao){
+            switch (opcao) {
                 case 'Configurar conta':
                     console.log('Selecionou configurar conta')
                     break
                 case 'Sair':
-                    console.log('Selecionou sair')
+                    Swal.fire({
+                        title: "Deseja sair da sua conta?",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#91C141",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Sim",
+                        reverseButtons: true,
+                        cancelButtonText: "Não",
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire({
+                                title: "Você saiu da sua conta",
+                                text: "Retornando à página inicial",
+                                icon: "success",
+                                timer: 2500,
+                                timerProgressBar: true,
+                                showConfirmButton: false,
+                            }).then(() => {
+                                router.push({ path: '/' })
+                            });
+                        }
+                    });
+                    // router.push({
+                    //     path: '/'
+                    // })
                     break
             }
         }
@@ -66,8 +102,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .navbar-brand img {
     max-width: 200px;
+}
+
+:deep(.v-list-item__spacer) {
+    width: 16px !important; /* Ajuste o valor conforme necessário */
 }
 </style>
