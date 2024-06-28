3126b13f6bd12a6cfa4388786f9c3c7e02a5e3d6
luizaboaventura <luizaboaventuram@gmail.com>
2024-06-27 21:39:51 -0300

    feat: adicionar dependentes dinamicamente

--- frontend/src/components/home/ModalRegister.vue
+++ frontend/src/components/home/ModalRegister.vue
@@ -28,30 +28,33 @@
               <v-text-field label="Confirmação da senha" type="password" required></v-text-field>
             </v-col>
           </v-row>
-          <div class="d-flex justify-space-between pt-1">
-            <h4 class="pt-4 pb-2 font-weight-bold">DEPENDENTE 1</h4>
-            <v-btn icon="mdi-delete" elevation="0"></v-btn>        
+          <h4 class="pt-2 font-weight-bold">DEPENDENTES</h4>
+          <div v-for="(dependent, index) in dependents" :key="index">
+            <div class="d-flex justify-space-between pt-1">
+              <h4 class="pt-4 pb-2">DEPENDENTE {{ index + 1 }}</h4>
+              <v-btn icon="mdi-delete" elevation="0" @click="removeDependent(index)"></v-btn>
+            </div>
+            <v-row>
+              <v-col cols="12" md="6" class="pb-0">
+                <v-text-field label="Nome completo" type="text" v-model="dependent.name" required></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6" class="pb-0">
+                <v-text-field label="E-mail" type="email" v-model="dependent.email" required></v-text-field>
+              </v-col>
+              <v-col cols="12" md="4" class="pb-0">
+                <v-text-field label="Grau de Parentesco" type="text" v-model="dependent.relationship" required></v-text-field>
+              </v-col>
+              <v-col cols="12" md="4" class="pb-0">
+                <v-text-field label="CPF" type="text" v-model="dependent.cpf" required></v-text-field>
+              </v-col>
+              <v-col cols="12" md="4" class="pb-0">
+                <v-text-field label="Telefone" type="text" v-model="dependent.phone" required></v-text-field>
+              </v-col>
+            </v-row>
           </div>
-          <v-row>
-            <v-col cols="12" md="6" class="pb-0">
-              <v-text-field label="Nome completo" type="text" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6" class="pb-0">
-              <v-text-field label="E-mail" type="email" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4" class="pb-0">
-              <v-text-field label="Grau de Parentesco" type="text" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4" class="pb-0">
-              <v-text-field label="CPF" type="text" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4" class="pb-0">
-              <v-text-field label="Telefone" type="text" required></v-text-field>
-            </v-col>
-            <v-col cols="12" class="text-center mb-2 mt-0">
-              <v-btn text elevation="0" rounded="0"><span class="mdi mdi-plus"></span>Adicionar</v-btn>
-            </v-col>
-          </v-row>
+          <v-col cols="12" class="text-center mb-2 mt-0">
+            <v-btn text elevation="0" rounded="0" @click="addDependent"><span class="mdi mdi-plus"></span>Adicionar</v-btn>
+          </v-col>
         </v-card-text>
         <v-card-actions class='d-flex justify-center'>
           <v-btn text rounded="0" @click="closeDialog">Cancelar</v-btn>
@@ -74,6 +77,15 @@ export default {
   data() {
     return {
       dialog: this.showModalRegister,
+      dependents: [
+        {
+          name: '',
+          email: '',
+          relationship: '',
+          cpf: '',
+          phone: ''
+        }
+      ]
     };
   },
   watch: {
@@ -89,6 +101,18 @@ export default {
   methods: {
     closeDialog() {
       this.$emit('close');
+    },
+    addDependent() {
+      this.dependents.push({
+        name: '',
+        email: '',
+        relationship: '',
+        cpf: '',
+        phone: ''
+      });
+    },
+    removeDependent(index) {
+      this.dependents.splice(index, 1);
     },
     saveItem() {
     }
